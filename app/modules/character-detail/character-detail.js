3f4e5711e5edf4649fc53ca95bc42cb24fdd6d41
JASON <jgraham84@gmail.com>
2014-06-03 13:26:35 -0400

    Adding stubbed out character detail stuff

---
+++ app/modules/character-detail/character-detail.js
@@ -0,0 +1,16 @@
+/* global define */
+define(function (require) {
+    "use strict";
+
+    // Require statements
+    var angular = require("angular");
+    var characterDetailController = require("modules/character-detail/character-detail-controller");
+
+    // Create character detail module
+    var characterDetail = angular.module("characterDetail", []);
+
+    // Define the controller
+    characterDetail.controller('CharacterDetailCtrl', characterDetailController);
+
+    return characterDetail;
+});
