6bd97cbf2c2e067809799ecd865ef5dd299cdcfb
asribhashyam <asribhashyam@summa-tech.com>
2014-06-10 13:26:17 -0400

    new encounter modal

--- app/modules/encounter/encounter-controller.js
+++ app/modules/encounter/encounter-controller.js
@@ -1,10 +1,11 @@
 define(function (require) {
     'use strict';
 
-    function EncounterController($state, $stateParams, ApiService) {
+    function EncounterController($state, $stateParams, $modal, ApiService) {
 
         this.$state = $state;
         this.$stateParams = $stateParams;
+        this.$modal = $modal;
         this.ApiService = ApiService;
 
         // Get the type passed in off of the route parameters
@@ -14,6 +15,36 @@ define(function (require) {
         this.selectedResult = ApiService.apiResults.encounters;
     }
 
+    EncounterController.prototype.openCreateModal = function() {
+        var self = this;
 
-    return ['$state', '$stateParams', 'ApiService', EncounterController];
+        console.log("clicked");
+
+        var ModalInstanceCtrl = function ($scope, $modalInstance) {
+
+            $scope.ok = function () {
+                $modalInstance.close();
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        };
+
+        var modalInstance = self.$modal.open({
+            templateUrl: "modules/encounter/newEncounter.html",
+            controller: ModalInstanceCtrl,
+            resolve: {
+
+            }
+        });
+
+        modalInstance.result.then(function () {
+            console.log("got here");
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    }
+
+    return ['$state', '$stateParams', '$modal', 'ApiService', EncounterController];
 });
