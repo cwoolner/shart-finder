13d1a7adb0bc7ef449e8b441d8958f739ddeb149
= <flavin@gmail.com>
2014-06-10 10:26:46 -0400

    Adding character details to stub and page

--- app/stubs/getCharacters.js
+++ app/stubs/getCharacters.js
@@ -3,39 +3,133 @@ define({
     "data": [
         {
             "_id" : "1",
-            "name": "El Volpe"
+            "name": "El Volpe",
+            "player": "Tom",
+            "sex": "Male",
+            "race": "Human",
+            "class": "Bard",
+            "level": "3",
+            "alignment": "Neutral Good",
+            "age": "18",
+            "height": "5' 4\"",
+            "weight": "140lb"
         },
         {
             "_id" : "2",
-            "name": "Sl'lab"
+            "name": "Sl'lab",
+            "player": "Jason",
+            "sex": "Male",
+            "race": "Half-Orc",
+            "class": "Barbarian",
+            "level": "3",
+            "alignment": "Chaotic Good",
+            "age": "17",
+            "height": "6' 2\"",
+            "weight": "222lb"
         },
         {
             "_id" : "3",
-            "name": "Krug"
+            "name": "Krug",
+            "player": "Chris",
+            "sex": "Male",
+            "race": "Human",
+            "class": "Fighter",
+            "level": "2",
+            "alignment": "True Neutral",
+            "age": "18",
+            "height": "5' 10\"",
+            "weight": "180lb"
         },
         {
             "_id" : "4",
-            "name": "Gandor"
+            "name": "Gandor",
+            "player": "Sug",
+            "sex": "Male",
+            "race": "Human",
+            "class": "Wizard",
+            "level": "3",
+            "alignment": "Chaotic Good",
+            "age": "18",
+            "height": "5' 4\"",
+            "weight": "140lb"
         },
         {
             "_id" : "5",
-            "name": "Baltok"
+            "name": "Baltok",
+            "player": "Dean",
+            "sex": "Male",
+            "race": "Dwarf",
+            "class": "Cleric",
+            "level": "3",
+            "alignment": "Chaotic Neutral",
+            "age": "51",
+            "height": "4'",
+            "weight": "164lb"
         },
         {
             "_id" : "6",
-            "name": "Dorion"
+            "name": "Dorion",
+            "player": "Mike",
+            "sex": "Male",
+            "race": "Dwarf",
+            "class": "Fighter",
+            "level": "3",
+            "alignment": "Chaotic Neutral",
+            "age": "58",
+            "height": "4' 1\"",
+            "weight": "178lb"
         },
         {
             "_id" : "7",
-            "name": "Bellamin"
+            "name": "Bellamin",
+            "player": "Ben",
+            "sex": "Male",
+            "race": "Halfling",
+            "class": "Rogue",
+            "level": "3",
+            "alignment": "Chaotic Neutral",
+            "age": "25",
+            "height": "2' 11\"",
+            "weight": "30lb"
         },
         {
             "_id" : "8",
-            "name": "Leithiandir"
+            "name": "Leithiandir",
+            "player": "Abhi",
+            "sex": "Male",
+            "race": "Elf",
+            "class": "Rogue",
+            "level": "3",
+            "alignment": "Chaotic Neutral",
+            "age": "124",
+            "height": "6' 1\"",
+            "weight": "117lb"
         },
         {
             "_id" : "9",
-            "name": "Miitya"
+            "name": "Miitya",
+            "player": "Andrew",
+            "sex": "Male",
+            "race": "Half-Elf",
+            "class": "Ranger",
+            "level": "3",
+            "alignment": "",
+            "age": "",
+            "height": "",
+            "weight": ""
+        },
+        {
+            "_id" : "10",
+            "name": "Loki",
+            "player": "Joe",
+            "sex": "Male",
+            "race": "Half-Elf",
+            "class": "Druid",
+            "level": "2",
+            "alignment": "True Neutral",
+            "age": "24",
+            "height": "5' 9\"",
+            "weight": "135lb"
         }
     ]
 });
